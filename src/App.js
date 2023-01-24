3c5355b75b68d11b4aa96f4ca4725a189960d300
Tsaqif <mushoddaqt@gmail.com>
2023-01-24 08:52:54 +0700

    moppin 2

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className="flex h-screen w-screen flex-col items-center justify-center bg-gradient-to-t from-slate-100 to-blue-200">
-          <h1 className="text-center font-mono font-semibold lg:text-5xl">sorry, tsaqif's web is being developed😁👍</h1>
-          <img className="w-56 lg:w-1/3" src="./fred-mopping-fred.gif" alt="hai" />
+          <h1 className="text-center font-mono font-semibold md:text-3xl lg:text-5xl">sorry, tsaqif's web is being developed😁👍</h1>
+          <img className="w-56 md:w-1/3" src="./fred-mopping-fred.gif" alt="hai" />
           <script type="text/javascript" async src="https://tenor.com/embed.js"></script>
         </div>
       </header>
